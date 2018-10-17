0eb094e64cb3831d6591578cb9cf18f3d3221e16
Alex <lexa.scorpion@mail.ru>
2018-10-17 21:09:43 +0300

    Механизм расчёта суммы. Пример расчёта суммы.

--- Calculator.php
+++ Calculator.php
@@ -19,9 +19,39 @@ class Calculator
 
     /**
      * @return array
+     * @throws Exception
      */
     public function calculate()
     {
-        return [];
+        $this->data['d14'] = $this->sumByCoordinates('d9', 'd12');
+
+        return $this->data;
+    }
+
+    /**
+     * @param string $from
+     * @param string $to
+     * @return int|mixed
+     *
+     * @throws Exception
+     */
+    private function sumByCoordinates($from, $to)
+    {
+        $min = preg_replace('/\D/', '', $from);
+        $max = preg_replace('/\D/', '', $to);
+        $letter = $from[0];
+        $sum = 0;
+
+        for ($i = $min; $i <= $max; $i++) {
+            $key = $letter . $i;
+
+            if (!array_key_exists($key, $this->data)) {
+                throw new Exception(sprintf('Key with name "%s" was not found', $key));
+            }
+
+            $sum += $this->data[$key];
+        }
+
+        return $sum;
     }
 }
